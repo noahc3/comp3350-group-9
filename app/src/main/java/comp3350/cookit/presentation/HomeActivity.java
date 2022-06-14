804c9095f7ad8ef95e306a3049ad486d1be75aca
Nyl S <nyldranreb78@gmail.com>
2022-06-13 20:25:38 -0500

    Restarted Activities and Layouts

--- app/src/main/java/comp3350/cookit/presentation/HomeActivity.java
+++ app/src/main/java/comp3350/cookit/presentation/HomeActivity.java
@@ -1,46 +1,21 @@
 package comp3350.cookit.presentation;
 
-import comp3350.cookit.R;
-import comp3350.cookit.application.Main;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
-public class HomeActivity extends Activity {
+import comp3350.cookit.R;
 
+public class HomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Main.startUp();
-
         setContentView(R.layout.activity_home);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        Main.shutDown();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
+    public void newRecipesOnClick(View v) {
+        Intent newRecipesIntent = new Intent(HomeActivity.this, NewRecipeActivity.class);
+        startActivity(newRecipesIntent);
     }
 }
---
+++ app/src/main/java/comp3350/cookit/presentation/NewRecipeActivity.java
@@ -0,0 +1,15 @@
+package comp3350.cookit.presentation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import comp3350.cookit.R;
+
+public class NewRecipeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_recipe);
+    }
+}
