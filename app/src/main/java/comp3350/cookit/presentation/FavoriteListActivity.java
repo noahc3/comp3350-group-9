2043d60fa85ab1fe373cbdc8676f2f6a373e9f3b
Kate <fef7005@gmail.com>
2022-07-28 02:50:31 -0500

    Update FavoriteListActivity.java

--- app/src/main/java/comp3350/cookit/presentation/FavoriteListActivity.java
+++ app/src/main/java/comp3350/cookit/presentation/FavoriteListActivity.java
@@ -4,12 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.List;
 
 import comp3350.cookit.R;
